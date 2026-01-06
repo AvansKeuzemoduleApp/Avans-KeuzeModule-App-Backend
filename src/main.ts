6aff0bb314ad4bf6ea3b06cc3978388975ea5436
Wasssiimm <w.balouda@student.avans.nl>
2026-01-06 15:34:35 +0100

    Changes to dockerfiles based on copilot reccommendations

--- src/main.ts
+++ src/main.ts
@@ -6,12 +6,8 @@ import { AllExceptionsFilter } from './filters/all-exceptions.filter';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-
-<<<<<<< feature/Modules-Endpoint
   app.setGlobalPrefix('api');
 
-=======
->>>>>>> develop
   const expressApp = app.getHttpAdapter().getInstance();
   expressApp.set('trust proxy', 'loopback');
 
