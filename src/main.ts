4eb1c952620ffc04701bc8ca5810815828d556ad
Christian Horler <csh.sammt@gmail.com>
2026-01-08 10:24:24 +0100

    Enforced HTTPS-Backend

--- src/main.ts
+++ src/main.ts
@@ -3,13 +3,37 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import cookieParser from 'cookie-parser';
 import { AllExceptionsFilter } from './filters/all-exceptions.filter';
+import helmet from "helmet";
+
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.setGlobalPrefix('api');
 
   const expressApp = app.getHttpAdapter().getInstance();
-  expressApp.set('trust proxy', 'loopback');
+  expressApp.set('trust proxy', 1);
+
+  // HSTS for production only
+  app.use(
+    helmet({
+      hsts: process.env.NODE_ENV === "production"
+        ? { maxAge: 15552000, includeSubDomains: true, preload: true }
+        : false,
+    }),
+  );
+
+  // Force HTTPS
+  app.use((req: any, res: any, next: any) => {
+    const xfProto = (req.headers["x-forwarded-proto"] ?? "").toString();
+    const isHttps = req.secure || xfProto === "https";
+
+    if (process.env.FORCE_HTTPS === "true" && !isHttps) {
+      const host = req.headers.host;
+      return res.redirect(301, `https://${host}${req.originalUrl}`);
+    }
+
+    next();
+  })
 
   app.use(cookieParser());
   app.useGlobalFilters(new AllExceptionsFilter());
